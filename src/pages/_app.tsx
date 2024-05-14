7c71d25443cfed0a9e265f95a91944b760249585
Akmal <akm.kxd@gmail.com>
2024-05-14 15:16:35 +0500

    init: slick slider lib

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css';
 import '@/styles/reset.css';
 import 'react-modern-drawer/dist/index.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import type { AppProps } from 'next/app';
 
