c49602734faf1183473042a3915b82abf9838d39
Akmal <akm.kxd@gmail.com>
2024-05-14 18:58:53 +0500

    init: react-accessible-accordion lib

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/reset.css';
 import 'react-modern-drawer/dist/index.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import 'react-accessible-accordion/dist/fancy-example.css';
 
 import type { AppProps } from 'next/app';
 import { SWRConfig } from 'swr';
