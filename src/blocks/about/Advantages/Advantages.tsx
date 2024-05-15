c1d0ce7e308145ae7349f7464fff1af2b11c72d6
Akmal <akm.kxd@gmail.com>
2024-05-15 16:53:52 +0500

    feat: create about Advantages block

---
+++ src/blocks/about/Advantages/Advantages.tsx
@@ -0,0 +1,36 @@
+import styles from './styles.module.sass';
+
+import Image from 'next/image';
+
+const Advantages = () => {
+  return (
+    <section>
+      <div className={styles.wrapper}>
+        <ul>
+          <li>
+            <Image src='/world.svg' alt='' width={50} height={50} />
+            <p>Предоставляем американский адрес для самостоятельных покупок</p>
+          </li>
+          <li>
+            <Image src='/socket.svg' alt='' width={50} height={50} />
+            <p>Помогаем выкупить товары с любого онлайн-магазина США</p>
+          </li>
+          <li>
+            <Image src='/cubs.svg' alt='' width={50} height={50} />
+            <p>Доставляем посылки по всему миру</p>
+          </li>
+          <li>
+            <Image src='/mailphone.svg' alt='' width={50} height={50} />
+            <p>Находимся на связи в формате 24/7</p>
+          </li>
+          <li>
+            <Image src='/user-review.svg' alt='' width={50} height={50} />
+            <p>Гарантируем персональный подход!</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Advantages;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,16 @@
+import { useIsClient, useMediaQuery } from 'usehooks-ts';
 import Head from 'next/head';
 
 import Hero from '@/blocks/about/Hero/Hero';
 import Warehouse from '@/blocks/about/Warehouse/Warehouse';
+import Advantages from '@/blocks/about/Advantages/Advantages';
 
 const About = () => {
+  const isClient = useIsClient();
+  const matches = {
+    576: useMediaQuery('(min-width: 576px)'),
+  };
+
   return (
     <>
       <Head>
@@ -14,6 +21,7 @@ const About = () => {
 
       <Hero />
       <Warehouse />
+      {isClient && !matches[576] && <Advantages />}
     </>
   );
 };
