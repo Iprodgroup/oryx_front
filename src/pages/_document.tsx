42391a2ad6ff9dcd2e4febebbd442746a9f3cb61
Akmal <akm.kxd@gmail.com>
2024-05-15 15:53:40 +0500

    refactor: change global head meta/link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <meta name='description' content='oryx' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,9 +13,6 @@ export default function Home() {
     <>
       <Head>
         <title>Доставка товаров из США в Казахстан - ORYX</title>
-        <meta name='description' content='oryx' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <Hero />
