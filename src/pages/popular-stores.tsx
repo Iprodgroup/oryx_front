2b8a183ed6178cf61391a5d3dd0cf9bbc7447fe2
Akmal <akm.kxd@gmail.com>
2024-05-16 16:08:40 +0500

    feat: create popular stores page

---
+++ src/pages/popular-stores.tsx
@@ -0,0 +1,58 @@
+import styles from '@/styles/PopularStores.module.sass';
+
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
+import Head from 'next/head';
+
+import { Category } from '@/types/category.interface';
+import instance from '@/utils/axios';
+
+export const getServerSideProps = (async () => {
+  const res = await instance.get('/categories');
+  const categories: Category[] = await res.data;
+
+  return { props: { categories } };
+}) satisfies GetServerSideProps<{ categories: Category[] }>;
+
+const PopularStores = ({
+  categories,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  return (
+    <>
+      <Head>
+        <title>Список популярных магазинов в США - ORYX</title>
+      </Head>
+
+      <section>
+        <div className={styles.wrapper}>
+          <div className={styles.top}>
+            <h2>Популярные магазины в США</h2>
+            <p>
+              Мы подготовили для вас список самых популярных магазинов одежды,
+              которые диктуют тренды каждого сезона
+            </p>
+            <ul>
+              {categories.map((category) => (
+                <li key={category.id}>{category.name}</li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.bottom}>
+            <h1>Что чаще всего покупают в США?</h1>
+            <p>
+              Сейчас огромной популярностью пользуются интернет-магазины в США.
+              В Америке можно купить все, что угодно от одежды, бытовой техники
+              и даже автомобильных запчастей. На покупке оригинальных брендов Вы
+              сэкономите 20-40% её офлайн стоимости, а на скидках можно
+              сэкономить до 80-90%, учитывая доставку. Помимо экономии,
+              интернет-шоппинг в США отличается огромным разнообразием
+              ассортимента и эксклюзивных коллекций, которые редко найдёшь в
+              нашей стране.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default PopularStores;
---
+++ src/types/category.interface.ts
@@ -0,0 +1,9 @@
+export interface Category {
+  id: number;
+  parent_id: number;
+  order: number;
+  name: string;
+  slug: string;
+  created_at: string;
+  updated_at: string;
+}
