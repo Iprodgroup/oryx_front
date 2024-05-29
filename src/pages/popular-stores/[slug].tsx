14388542000246a22d5999533d942834e11a49bc
Akmal <akm.kxd@gmail.com>
2024-05-29 21:22:40 +0500

    feat: implement store

---
+++ src/pages/popular-stores/[slug].tsx
@@ -0,0 +1,32 @@
+import styles from '@/styles/Store.module.sass';
+
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
+import Image from 'next/image';
+
+import { Store as IStore } from '@/types/store.interface';
+import instance from '@/utils/axios';
+
+export const getServerSideProps = (async ({ query }) => {
+  const res = await instance.get<{ store: IStore }>(`/store/${query.slug}`);
+  const store = res.data.store;
+
+  return { props: { store } };
+}) satisfies GetServerSideProps<{ store: IStore }>;
+
+const Store = ({
+  store,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  return (
+    <section>
+      <div className={styles.wrapper}>
+        <h1>Информация о {store.name}</h1>
+        <Image src={store.img} alt='' width={350} height={350} />
+        <a href={store.link} target='_blank' className={styles.btn}>
+          Перейти на сайт
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Store;
