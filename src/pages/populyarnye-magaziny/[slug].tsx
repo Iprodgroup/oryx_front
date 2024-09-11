4415a2a41bd920c9c5ae44c986e829d3e8e1632a
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-09-11 17:13:11 +0500

    descr for brands

--- src/pages/populyarnye-magaziny/[slug].tsx
+++ src/pages/populyarnye-magaziny/[slug].tsx
@@ -1,10 +1,10 @@
-import styles from '@/styles/Store.module.sass';
+import styles from "@/styles/Store.module.sass";
 
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
-import Image from 'next/image';
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
+import Image from "next/image";
 
-import { Store as IStore } from '@/types/store.interface';
-import instance from '@/utils/axios';
+import { Store as IStore } from "@/types/store.interface";
+import instance from "@/utils/axios";
 
 export const getServerSideProps = (async ({ query }) => {
   const res = await instance.get<{ store: IStore }>(`/store/${query.slug}`);
@@ -20,8 +20,12 @@ const Store = ({
     <section>
       <div className={styles.wrapper}>
         <h1>Информация о {store.name}</h1>
-        <Image src={store.img} alt='' width={350} height={350} />
-        <a href={store.link} target='_blank' className={styles.btn}>
+        <div className={styles.content}>
+          <Image src={store.img} alt="" width={350} height={350} />
+          <div dangerouslySetInnerHTML={{__html: store.short_desc}}></div>
+        </div>
+
+        <a href={store.link} target="_blank" className={styles.btn}>
           Перейти на сайт
         </a>
       </div>
