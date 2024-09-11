c5805c465cacc52b12439db46beaa61e21824176
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-09-11 16:32:44 +0500

    popoular canonicls

--- src/blocks/home/PopularStores/PopularStores.tsx
+++ src/blocks/home/PopularStores/PopularStores.tsx
@@ -1,34 +1,35 @@
-import styles from './styles.module.sass';
+import styles from "./styles.module.sass";
 
-import Slider from 'react-slick';
-import useSWR from 'swr';
-import Image from 'next/image';
-import Link from 'next/link';
-import classNames from 'classnames';
+import Slider from "react-slick";
+import useSWR from "swr";
+import Image from "next/image";
+import Link from "next/link";
+import classNames from "classnames";
 
-import { Store } from '@/types/store.interface';
-import instance from '@/utils/axios';
-import settings from './slider.settings';
+import { Store } from "@/types/store.interface";
+import instance from "@/utils/axios";
+import settings from "./slider.settings";
 
 const fetcher = (arg: string): Promise<{ stores: Store[] }> =>
   instance(arg).then((res) => res.data);
 
 const PopularStores = () => {
-  const { data } = useSWR('/popular-stores', fetcher);
+  const { data } = useSWR("/popular-stores", fetcher);
 
   return (
     <section>
+      <link rel="canonical" href="https://oryx.kz/populyarnye-magaziny" />
       <div className={styles.wrapper}>
         <h2>Популярные магазины</h2>
         <Slider
           {...settings}
-          className={classNames('stores-slider', styles.slider)}
+          className={classNames("stores-slider", styles.slider)}
         >
           {data?.stores.map((store) => (
             <Link key={store.id} href={`/populyarnye-magaziny/${store.slug}`}>
               <Image
                 src={store.img}
-                alt=''
+                alt=""
                 width={170}
                 height={170}
                 className={styles.slider__img}
@@ -36,7 +37,7 @@ const PopularStores = () => {
             </Link>
           ))}
         </Slider>
-        <Link href='/populyarnye-magaziny' className={styles.all__btn}>
+        <Link href="/populyarnye-magaziny" className={styles.all__btn}>
           Смотреть все
         </Link>
       </div>
