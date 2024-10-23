679b04c9bca170040e04d39c6b21c696c42acf06
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-10-23 10:22:38 +0500

    added google tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,30 +28,8 @@ export default function Document() {
         />
         <meta property="og:image" content="https://oryx.kz/logo.svg" />
 
-        <link
-          rel="alternate"
-          href="https://oryx.kz"
-          hrefLang="x-default"
-        />
-        <link
-          rel="alternate"
-          href="https://oryx.kz"
-          hrefLang="en-ru"
-        />
-
-        {/* Twitter Meta Tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:title"
-          content="Доставка товаров из США в Казахстан - ORYX"
-        />
-        <meta
-          name="twitter:description"
-          content="Надежная доставка посылок и товаров из США в Казахстан от компании ORYX. ✈ Услуги мейлфорвардера: удобный заказ, отслеживание и быстрая доставка посылок из США. 🚚
-"
-        />
-        <meta name="twitter:image" content="https://oryx.kz/logo.svg" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="alternate" href="https://oryx.kz" hrefLang="x-default" />
+        <link rel="alternate" href="https://oryx.kz" hrefLang="en-ru" />
 
         {/* Google Analytics */}
         <script
@@ -68,121 +46,50 @@ export default function Document() {
             `,
           }}
         />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "Organization",
-              name: "ORYX",
-              url: "https://oryx.kz",
-              logo: "https://oryx.kz/logo.svg",
-              sameAs: ["https://www.instagram.com/oryx.usa.kz/"],
-              contactPoint: [
-                {
-                  "@type": "ContactPoint",
-                  telephone: "+7-700-323-22-22",
-                  contactType: "Customer Service",
-                  areaServed: "KZ",
-                  availableLanguage: "Russian",
-                },
-                {
-                  "@type": "ContactPoint",
-                  telephone: "+7-747-515-56-13",
-                  contactType: "Customer Service",
-                  areaServed: "KZ",
-                  availableLanguage: "Russian",
-                },
-              ],
-            }),
-          }}
-        />
-        {/* <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "BreadcrumbList",
-              itemListElement: [
-                {
-                  "@type": "ListItem",
-                  position: 1,
-                  name: "Главная",
-                  item: "https://oryx.kz/",
-                },
-                {
-                  "@type": "ListItem",
-                  position: 2,
-                  name: "О нас",
-                  item: "https://oryx.kz/o-kompanii",
-                },
-                {
-                  "@type": "ListItem",
-                  position: 3,
-                  name: "Популярные магазины",
-                  item: "https://oryx.kz/populyarnye-magaziny",
-                },
-                {
-                  "@type": "ListItem",
-                  position: 4,
-                  name: "Купи вместо меня",
-                  item: "https://oryx.kz/buy-me",
-                },
-                {
-                  "@type": "ListItem",
-                  position: 5,
-                  name: "Контакты",
-                  item: "https://oryx.kz/kontakty",
-                },
-              ],
-            }),
-          }}
-        /> */}
         {/* Yandex Metrica */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function (m, e, t, r, i, k, a) {
-                m[i] =
-                  m[i] ||
-                  function () {
-                    (m[i].a = m[i].a || []).push(arguments);
-                  };
-                m[i].l = Number(new Date()); // Explicitly convert Date to number
-                for (let j = 0; j < document.scripts.length; j++) {
-                  if (document.scripts[j].src === r) {
-                    return;
-                  }
-                }
-                k = e.createElement(t);
-                a = e.getElementsByTagName(t)[0];
-                k.async = 1;
-                k.src = r;
-                a.parentNode.insertBefore(k, a);
-              })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0];
+              k.async=1;k.src=r;a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
               ym(98298866, "init", {
-                clickmap: true,
-                trackLinks: true,
-                accurateTrackBounce: true,
-                webvisor: true,
-                ecommerce: "dataLayer"
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true,
+                ecommerce:"dataLayer"
               });
             `,
           }}
         />
-        {/* Yandex Metrica noscript */}
-        <noscript>
-          <div>
-            <img
-              src="https://mc.yandex.ru/watch/98298866"
-              style={{ position: "absolute", left: "-9999px" }}
-              alt="yandex_watch"
-            />
-          </div>
-        </noscript>
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PZJV3C4K"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PZJV3C4K');
+            `,
+          }}
+        />
+
         <Main />
         <NextScript />
       </body>
