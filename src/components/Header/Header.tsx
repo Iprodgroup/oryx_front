bc3e912102b8188fadea9cee80eead46dd2c0725
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-11-11 16:13:14 +0500

    fix header & footer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -28,14 +28,12 @@ const Header = () => {
         const token = Cookies.get("access_token");
 
         if (token) {
-          // Отправляем запрос с заголовком Authorization
           const response = await instance.get("/profile/balance", {
             headers: {
-              Authorization: `Bearer ${token}`
-            }
+              Authorization: `Bearer ${token}`,
+            },
           });
-            setBalance(response.data.balance);
-          
+          setBalance(response.data.balance);
         } else {
           console.error("Токен не найден");
         }
@@ -53,7 +51,7 @@ const Header = () => {
     if (balance === null) return "gray";
     if (balance < 0) return "red";
     if (balance === 0.00) return "gray";
-    if(balance > 0) return "green";
+    if (balance > 0) return "green";
   };
 
   const whatUrl = () => {
@@ -78,76 +76,87 @@ const Header = () => {
     }
   };
 
-  return (
-    isClient && (
-      <header className={styles.header}>
-        <div className={styles.wrapper}>
-          <div className={styles.left}>
-            <Link href="/">
-              {matches[576] ? (
-                <Image
-                  src="/logo.svg"
-                  alt="logo"
-                  width={170}
-                  height={70}
-                  priority
-                />
-              ) : (
-                <LogoIcon />
-              )}
-            </Link>
-          </div>
-          {matches[1200] && (
-            <div className={styles.center}>
-              <nav>
-                <ul>
-                  <li>
-                    <Link href="/o-kompanii">О нас</Link>
-                  </li>
-                  <li>
-                    <Link href="/populyarnye-magaziny">
-                      Популярные магазины
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="/buy-me">Купи вместо меня</Link>
-                  </li>
-                  <li>
-                    <Link href="/kontakty">Контакты</Link>
-                  </li>
-                  <li>
-                    <Link href="/#calculator">Калькулятор</Link>
-                  </li>
-                  <li>
-                    <Link href="/faq">Q&A</Link>
-                  </li>
-                </ul>
-              </nav>
-            </div>
+  // Статичный контент, который рендерится без JavaScript
+  const staticContent = (
+    <>
+      <div className={styles.left}>
+        <Link href="/">
+          {matches[576] ? (
+            <Image
+              src="/logo.svg"
+              alt="logo"
+              width={170}
+              height={70}
+              priority
+            />
+          ) : (
+            <LogoIcon />
           )}
-          <div className={styles.right}>
-            {isAuthenticated ? (
-              <>
-                {whatUrl()}
-                <Link href="/logout" className={styles.register__btn}>
-                  Выход
+        </Link>
+      </div>
+      {matches[1200] && (
+        <div className={styles.center}>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/o-kompanii">О нас</Link>
+              </li>
+              <li>
+                <Link href="/populyarnye-magaziny">
+                  Популярные магазины
                 </Link>
-              </>
-            ) : (
-              <>
-                <Link href="/login" className={styles.login__btn}>
-                  <Image src="/login.svg" alt="login" width={16} height={20} /> Вход
-                </Link>
-                <Link href="/register" className={styles.register__btn}>
-                  Регистрация
-                </Link>
-              </>
-            )}
-            {!matches[1200] && <Drawer />}
-          </div>
+              </li>
+              <li>
+                <Link href="/buy-me">Купи вместо меня</Link>
+              </li>
+              <li>
+                <Link href="/kontakty">Контакты</Link>
+              </li>
+              <li>
+                <Link href="/#calculator">Калькулятор</Link>
+              </li>
+              <li>
+                <Link href="/faq">Q&A</Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-      </header>
-    )
+      )}
+    </>
+  );
+
+  return (
+    <>
+      {isClient && (
+        <header className={styles.header}>
+          <div className={styles.wrapper}>
+            {/* Статичный контент */}
+            {staticContent}
+
+            <div className={styles.right}>
+              {isAuthenticated ? (
+                <>
+                  {whatUrl()}
+                  <Link href="/logout" className={styles.register__btn}>
+                    Выход
+                  </Link>
+                </>
+              ) : (
+                <>
+                  <Link href="/login" className={styles.login__btn}>
+                    <Image src="/login.svg" alt="login" width={16} height={20} /> Вход
+                  </Link>
+                  <Link href="/register" className={styles.register__btn}>
+                    Регистрация
+                  </Link>
+                </>
+              )}
+              {!matches[1200] && <Drawer />}
+            </div>
+          </div>
+        </header>
+      )}
+    </>
   );
 };
 
