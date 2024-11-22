e0ad854848d77c94f5c4eb1c4a02032db0a49cdc
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-11-22 10:41:00 +0500

    test pay method

--- src/components/AddBalance/AddBalance.tsx
+++ src/components/AddBalance/AddBalance.tsx
@@ -4,9 +4,17 @@ import Link from "next/link";
 
 const AddBalance = () => {
   return (
+    <>
     <Link href="https://cms.oryx.kz/pay" target="_blank" className={styles.btn}>
       Пополнить баланс
     </Link>
+    <form action="/pay" method="GET">
+      <label htmlFor="amount">Сумма:</label>
+      <input type="number" name="amount" id="amount" min="0" step="0.01" required/>
+      <button type="submit" style={{ marginTop: "10px", padding: "10px 20px", background: "#EB3738", color: "white" }}>Оплатить</button>
+    </form>
+    </>
+    
   );
 };
 
