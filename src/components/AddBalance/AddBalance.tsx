7d9916889fe0f26ae0253edc7c16d9ca3a894196
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-11-22 11:24:42 +0500

    implement add balance cash

--- src/components/AddBalance/AddBalance.tsx
+++ src/components/AddBalance/AddBalance.tsx
@@ -1,22 +1,53 @@
+import { Box } from "@mui/material";
 import styles from "./styles.module.sass";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import { useState } from "react";
 
-import Link from "next/link";
-
-const AddBalance = () => {
-  return (
-    <>
-      <Link href="https://cms.oryx.kz/pay" target="_blank" className={styles.btn}>
-        Пополнить баланс
-      </Link>
-      <form action="https://cms.oryx.kz/pay" method="GET">
-        <label htmlFor="amount">Сумма:</label>
-        <input type="number" name="amount" id="amount" min="0" step="0.01" required/>
-        <button type="submit" style={{ marginTop: "10px", padding: "10px 20px", background: "#EB3738", color: "white" }}>Оплатить</button>
-      </form>
-    </>
-    
-  );
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
 };
 
+const AddBalance = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <button onClick={handleOpen} className={styles.btn}>Пополнить баланс</button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <form action="https://cms.oryx.kz/pay" target="_blank" method="GET" style={{ display: "flex", flexDirection: "column" }}>
+            <label htmlFor="amount" style={{fontSize: "16px", fontWeight: "600"}}>Сумма:</label>
+            <input
+              type="number"
+              name="amount"
+              id="amount"
+              min="0"
+              step="0.01"
+              required
+              style={{ padding: "10px", border: "1px solid #ccc", borderRadius: "5px", margin: "10px 0" }}
+            />
+            <button type="submit" style={{ marginTop: "10px", width: "100%", padding: "10px 20px", backgroundColor: "#E65A57", color: "white", border: "none", borderRadius: "5px", cursor: "pointer" }}>Оплатить</button>
+          </form>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
 
 export default AddBalance;
