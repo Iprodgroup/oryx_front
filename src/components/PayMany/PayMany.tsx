cec9c3b0ab7631386740b9428f0a4963a7d79be5
Oleg Shakurov <thorsonodin2003@gmail.com>
2025-01-22 17:27:53 +0500

    pay many

---
+++ src/components/PayMany/PayMany.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.sass";
+
+const PayMany = () => {
+  return (
+    <div className={styles.btn}>
+      <p>Оплатить всё</p>
+    </div>
+  );
+};
+
+export default PayMany;
